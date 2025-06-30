fa845a4a60fb592594004603ca928b080355f1bb
Hakimei <kimei.truong88@gmail.com>
2025-06-30 14:28:05 +0700

    🎨 chore(tokens): Update primary color and rename tokens for consistency in SCSS and JSON files

---
+++ src/ts/tokens.ts
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export const ColorPrimary = "#ff890d";
+export const ColorSecondary = "#480ce0";
+export const ColorSurface = "#e00c8f";
+export const ColorBackground = "#d9e2e3";
+export const SpacingSmall = "8px";
+export const SpacingMedium = "16px";
+export const SpacingLarge = "24px";
+export const SpacingLarge1x = "32px";
+export const SpacingLarge2x = "48px";
